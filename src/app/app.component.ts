57cee3f3ed386791bc0ae3dac28914d8262fc0eb
tonitomystery <amaristany01@gmail.com>
2024-05-20 12:41:05 -0400

    feat: add services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageService } from './services/image.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cornerstone-angular17';
+
+  imageService = inject(ImageService);
 }
